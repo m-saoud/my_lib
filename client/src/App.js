574d058b5f6ebc1093f8a91be4ae8f9b1decdd39
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-10-03 21:20:24 +0900

    Start Client Side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import InputBook from './components/InputBook';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <InputBook/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/InputBook.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputBook = () => {
+  return (
+    <div>
+      <h1>InputBook</h1>
+    </div>
+  )
+}
+
+export default InputBook
