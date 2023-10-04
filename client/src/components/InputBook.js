cc813c6b64bd4764e25cc8458d48856b4b7fa34e
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-10-04 23:10:26 +0900

    Adjust CSS

--- client/src/components/InputBook.js
+++ client/src/components/InputBook.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
 const InputBook = () => {
   return (
-    <div>
-      <h1>InputBook</h1>
-    </div>
-  )
-}
+    <Fragment>
+      <h1 className="text-center">MY LIBRARY</h1>
+      <form className="d-flex ">
+        <input type="text" className="form-control width "></input>
+        <button className="btn btn-outline-secondary">Add</button>
+      </form>
+    </Fragment>
+  );
+};
 
-export default InputBook
+export default InputBook;
